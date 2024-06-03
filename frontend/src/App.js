de65a4c8915b099c140b4fe8a1fff5b127d5d434
Ismail Patel <100822861+ismailp145@users.noreply.github.com>
2024-06-02 19:55:53 -0500

    Created SignUp and CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './SignUp.css';
+import SignUp from './SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <><div className="App">
+
+      {<header className="App-header">
+        <img className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,9 +20,20 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
+
+      </header>}
+
+
+
+
+    </div><SignUp></SignUp></>
+
   );
+
 }
 
 export default App;
+/* 
+this went in between the header tags
+
+*/
---
+++ frontend/src/SignUp.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+// import React from 'react';
+
+function SignUp() {
+  return (
+    <form>
+      <label>
+        Name:
+        <input type="text" name="name" />
+      </label>
+      <label>
+        Email:
+        <input type="email" name="email" />
+      </label>
+      <label>
+        Password:
+        <input type="password" name="password" />
+      </label>
+      <input type="submit" value="Sign Up" />
+    </form>
+  );
+}
+
+export default SignUp;
