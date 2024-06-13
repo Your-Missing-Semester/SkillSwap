de2be6c752ff8a3cacb4547fb06a548070085a11
Aakash Shah <aakashshah714@gmail.com>
2024-06-12 21:16:28 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,29 @@
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Footer from "./components/Footer.js";
+import LandingPage from "./components/LandingPage.js";
+import Navbar from "./components/Navbar.js";
+import About from "./pages/About.js";
+import Home from "./pages/Home.js";
 import ChangeInfo from './Change/ChangeInfo';
 
 function App() {
-  return (
-    <div>
-      <ChangeInfo/>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Navbar />
+				<LandingPage />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					//<Route path="/about" element={<About />} />
+  				<Route path="/changeInfo" element={<ChangeInfo />} />
+				</Routes>
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
