763d26647d02e1c5a8b6433eaba814bbb4666125
Aakash Shah <aakashshah714@gmail.com>
2024-06-21 20:47:41 -0400

    Removed Comments and Cleaned up tabs/spacing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,29 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
 import Footer from "./components/Footer.js";
 import LandingPage from "./components/LandingPage.js";
 import Navbar from "./components/Navbar.js";
 import About from "./pages/About.js";
 import Home from "./pages/Home.js";
-import ChangeInfo from './Change/ChangeInfo';
+import ChangeInfo from "./Change/ChangeInfo";
 
 function App() {
-	return (
-		<Router>
-			<div className="App">
-				<Navbar />
-				<LandingPage />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					//<Route path="/about" element={<About />} />
-  				<Route path="/changeInfo" element={<ChangeInfo />} />
-				</Routes>
-				<Footer />
-			</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <LandingPage />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/changeInfo" element={<ChangeInfo />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
+
