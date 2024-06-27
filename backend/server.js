9a5c487695aac4ca4d923d27c5b6b852e1a81692
Nigel McAfee <nigel.mcafee@gmail.com>
2024-06-26 22:46:37 -0700

    add server

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+app.get("/", function (req, res) {
+    res.send("SMILE! :D");
+  });
+
+
+app.listen(port, function () {
+    console.log(`SkillSwap server listening on port ${port}`);
+  });
+
