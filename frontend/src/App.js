8f2b25ac47bd2637e231a2bbf67e272e17d4b2fa
Ismail Patel <100822861+ismailp145@users.noreply.github.com>
2024-06-29 20:09:32 -0500

    fixed css/ other issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-// import logo from './logo.svg';
-// import './App.css
-import './SignUp.module.css';
-import SignUp from './SignUp';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Footer from "./components/Footer.js";
+import LandingPage from "./components/LandingPage.js";
+import Navbar from "./components/Navbar.js";
+import About from "./pages/About.js";
+import Home from "./pages/Home.js";
 
 function App() {
-  return (
-  
-
-      
-
-
-
-
-    <SignUp></SignUp>
-
-  );
-
+	return (
+		<Router>
+			<div className="App">
+				<Navbar />
+				<LandingPage />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+				</Routes>
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
 export default App;
-/* 
-this went in between the header tags
-
-*/
--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -1,48 +1,36 @@
 import React from 'react';
-import './SignUp.module.css';
-
+import styles from './SignUp.module.css'; // Adjusted import to use the styles object
 
 function SignUp() {
-  return (    
-    <div >
+  return (
+    <div>
 
-    <form className='form'>
-
+      <form className={styles.form}> {/* Using styles object */}
         <h1>Sign Up</h1>
-        
-        <div className='form-group'>
 
-        <label>Email:</label>
-            <input type="email" name="email" />
-            
-        </div>
-        
-        <div className='form-group'>
-        <label>Password:</label>
-        <input type="password" name="password" />
-        </div>
-        
-        
-        <div className='form-group'>
-        <label>Retype Password: </label>
-        <input type="password" name="password" />
-        </div>
-       
-        <div className='submit-Option'>
-        <input type="submit" value="Sign Up" />
 
-        <button className="logIn-Button">
-          Log In
+        <div className={styles['form-group']}> {/* Using styles object with hyphenated class name */}
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="ReType Password" />
+        </div>
+
+        <div className={styles['submit-Option']}>
+          
+        <button className={styles['signUp-Button']}>
+            Sign Up
           </button>
+
+
+          <button className={styles['logIn-Button']}>
+            Log In
+          </button>
+          
         </div>
+      </form>
 
-
-    </form>
-  
-    </div> 
-
+    </div>
   );
-
 }
 
 export default SignUp;
