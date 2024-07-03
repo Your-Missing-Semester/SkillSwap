e0ec28a68bd05bdd0898d94e6b810a02d0b84d95
Ismail Patel <100822861+ismailp145@users.noreply.github.com>
2024-07-03 11:58:30 -0500

    prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Footer from "./components/Footer.js";
-import LandingPage from "./components/LandingPage.js";
-import Navbar from "./components/Navbar.js";
-import About from "./pages/About.js";
-import Home from "./pages/Home.js";
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Footer from './components/Footer.js';
+import LandingPage from './components/LandingPage.js';
+import Navbar from './components/Navbar.js';
+import About from './pages/About.js';
+import Home from './pages/Home.js';
 
 function App() {
-	return (
-		<Router>
-			<div className="App">
-				<Navbar />
-				<LandingPage />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/about" element={<About />} />
-				</Routes>
-				<Footer />
-			</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <LandingPage />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
