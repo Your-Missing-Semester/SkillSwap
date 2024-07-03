35d95c34bff71ce704fedf75b06a0d8d2c483b58
Ismail Patel <100822861+ismailp145@users.noreply.github.com>
2024-07-03 10:10:46 -0500

    used prettier

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -4,31 +4,23 @@ import styles from './SignUp.module.css'; // Adjusted import to use the styles o
 function SignUp() {
   return (
     <div>
-
-      <form className={styles.form}> {/* Using styles object */}
+      <form className={styles.form}>
+        {' '}
+        {/* Using styles object */}
         <h1>Sign Up</h1>
-
-
-        <div className={styles['form-group']}> {/* Using styles object with hyphenated class name */}
+        <div className={styles['form-group']}>
+          {' '}
+          {/* Using styles object with hyphenated class name */}
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
           <input type="password" placeholder="ReType Password" />
         </div>
-
         <div className={styles['submit-Option']}>
-          
-        <button className={styles['signUp-Button']}>
-            Sign Up
-          </button>
+          <button className={styles['signUp-Button']}>Sign Up</button>
 
-
-          <button className={styles['logIn-Button']}>
-            Log In
-          </button>
-          
+          <button className={styles['logIn-Button']}>Log In</button>
         </div>
       </form>
-
     </div>
   );
 }
