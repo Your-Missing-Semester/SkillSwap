71806984c3145623840de99c8097c9eb5474a6b1
Ismail Patel <100822861+ismailp145@users.noreply.github.com>
2024-07-09 13:27:59 -0500

    edited App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,21 @@ import LandingPage from './components/LandingPage.js';
 import Navbar from './components/Navbar.js';
 import About from './pages/About.js';
 import Home from './pages/Home.js';
-import SignUp from './components/SignUp.jsx';
+import SignUp from './components/SignUp/SignUp.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
+
       <Routes>
+        <Route path="/about" element={<About />} />
         <Route path="/" element={<LandingPage />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
