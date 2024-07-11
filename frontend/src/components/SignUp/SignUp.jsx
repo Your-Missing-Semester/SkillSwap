a186dbdd7be60bc37a2182bf207d413aab6a98ab
Ismail Patel <100822861+ismailp145@users.noreply.github.com>
2024-07-11 14:15:31 -0500

    resolved nit issues

--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -5,20 +5,16 @@ function SignUp() {
   return (
     <div>
       <form className={styles.form}>
-        {' '}
-        {/* Using styles object */}
         <h1>Sign Up</h1>
         <div className={styles['form-group']}>
-          {' '}
-          {/* Using styles object with hyphenated class name */}
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
           <input type="password" placeholder="ReType Password" />
         </div>
-        <div className={styles['submit-Option']}>
-          <button className={styles['signUp-Button']}>Sign Up</button>
+        <div className={styles['submit-option']}>
+          <button className={styles['sign-up-button']}>Sign Up</button>
 
-          <button className={styles['logIn-Button']}>Log In</button>
+          <button className={styles['log-in-button']}>Log In</button>
         </div>
       </form>
     </div>
