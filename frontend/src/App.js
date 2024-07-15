99c3648163f3db740ae3e7d43b8d040df81b0287
Nigel McAfee <nigel.mcafee@gmail.com>
2024-07-14 23:03:38 -0700

    fix Component directory path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Footer from './components/Footer.js';
-import LandingPage from './components/LandingPage.js';
-import Navbar from './components/Navbar.js';
+import Footer from './Components/Footer.js';
+import LandingPage from './Components/LandingPage.js';
+import Navbar from './Components/Navbar.js';
 import About from './pages/About.js';
 import Home from './pages/Home.js';
-import SignUp from './components/SignUp/SignUp.jsx';
+import SignUp from './Components/SignUp/SignUp.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import LoginForm from './components/LoginForm/LoginForm.jsx';
+import LoginForm from './Components/LoginForm/LoginForm.jsx';
 
 const App = () => {
   return (
