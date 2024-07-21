197468569979d1009c2f820fd9af29e412d40cfc
Ismail Patel <100822861+ismailp145@users.noreply.github.com>
2024-07-21 14:39:58 -0500

    edited server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const axios = require("axios");
+const session = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
 
 app.get("/", function (req, res) {
   res.send("SMILE! :D");
@@ -19,10 +23,13 @@ app.post("/api/sign-up", async function (req, res) {
     return res.status(400).send("Username and password are required");
   }
 
-  const userExists = await database.userExists(username);
+  // const userExists = await database.userExists(username);
+
   if (userExists) {
     return res.status(400).send("Username is already taken");
   }
+
+  await database.createUser(username, password);
 
   res.send("Sign up successful!");
 });
