35ab5780b74ebeef13c97a6c590d1b1a0ca9f45f
Ismail Patel <100822861+ismailp145@users.noreply.github.com>
2024-07-21 17:47:23 -0500

    idk

--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -5,10 +5,15 @@ import axios from 'axios';
 const SignUp = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (password !== confirmPassword) {
+      setError('Passwords do not match!');
+      return;
+    }
     try {
       const response = await axios.post('http://localhost:3000/api/sign-up', {
         username,
@@ -38,7 +43,12 @@ const SignUp = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <input type="password" placeholder="ReType Password" />
+          <input
+            type="password"
+            placeholder="ReType Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
         </div>
         <div className={styles['submit-option']}>
           <button className={styles['sign-up-button']}>Sign Up</button>
