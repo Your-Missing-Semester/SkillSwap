2abc0065b418149729f724e0a111bc539dee191e
Nigel McAfee <nigel.mcafee@gmail.com>
2024-07-21 12:44:15 -0700

    fix issues on found on main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Footer from './components/Footer.js';
-import LandingPage from './components/LandingPage.js';
-import Navbar from './components/Navbar.js';
+import Footer from './Components/Footer.js';
+import LandingPage from './Components/LandingPage.js';
+import Navbar from './Components/Navbar.js';
 import About from './pages/About.js';
 import Home from './pages/Home.js';
-import SignUp from './components/SignUp/SignUp.jsx';
+import SignUp from './Components/SignUp/SignUp.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import LoginForm from './components/LoginForm/LoginForm.jsx';
-import ChangeInfo from "./Change/ChangeInfo";
+import LoginForm from './Components/LoginForm/LoginForm.jsx';
+import ChangeInfo from "./Components/Change/ChangeInfo.jsx";
 
 
 const App = () => {
@@ -22,7 +22,7 @@ const App = () => {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
         <Route path="log-in" element={<LoginForm />} />
-        <Route path="change" element=(<ChangeInfo />} />
+        <Route path="change" element={<ChangeInfo />} />
       </Routes>
     </BrowserRouter>
 
